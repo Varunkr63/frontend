82ecd710dfe0d36f072b11f5d35c337b72dd43a7
Varun Kumar <varunkumar63931@gmail.com>
2024-02-25 18:35:26 +0530

    next js

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>my next.js app</h1>
+      <img src="https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Hyundai-Creta-180120241405.jpg&w=872&h=578&q=75&c=1" alt="" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae iusto possimus quod quasi cupiditate asperiores sit ad veritatis praesentium perferendis?</p>
+    </div>
+  )
+}
+
+export default Home
